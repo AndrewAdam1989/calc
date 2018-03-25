1f2bf392e9af6bb9d7e1973e5212dabb84841760
AndrewAdam1989 <36536198+AndrewAdam1989@users.noreply.github.com>
2018-03-25 18:39:26 +0800

    Add files via upload

---
+++ entrypoint.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+SS_CONFIG=${SS_CONFIG:-""}
+SS_MODULE=${SS_MODULE:-"ss-server"}
+OBFS_CONFIG=${OBFS_CONFIG:-""}
+OBFS_MODULE=${OBFS_MODULE:-"obfs-server"}
+KCP_CONFIG=${KCP_CONFIG:-""}
+KCP_MODULE=${KCP_MODULE:-"kcpserver"}
+
+while getopts "s:S:o:O:k:K" OPT; do
+    case $OPT in
+        s)
+            SS_CONFIG=$OPTARG;;
+        S)
+            SS_MODULE=$OPTARG;;
+        o)
+            OBFS_CONFIG=$OPTARG;;
+        O)
+            OBFS_MODULE=$OPTARG;;
+        k)
+            KCP_CONFIG=$OPTARG;;
+        K)
+            KCP_MODULE=$OPTARG;;
+    esac
+done
+
+if [ "$SS_CONFIG" != "" ]; then
+    $SS_MODULE $SS_CONFIG 2>&1 &
+fi
+
+if [ "$OBFS_CONFIG" != "" ]; then
+    $OBFS_MODULE $OBFS_CONFIG 2>&1 &
+fi
+
+if [ "$KCP_CONFIG" != "" ]; then
+    $KCP_MODULE $KCP_CONFIG 2>&1 &
+fi
